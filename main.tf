4849e672f204418f8c7ccd899c1c8f90599066ae
Will_Moura <william@febrafar.com.br>
2026-09-15 14:26:04 -0300

    temporario

--- main.tf
+++ main.tf
@@ -65,8 +65,3 @@ resource "aws_db_instance" "mysql" {
   publicly_accessible    = true
   storage_encrypted      = false # Evita rejeição de KMS key no Academy
 }
-
-import {
-  to = aws_db_instance.mysql
-  id = "tc-soat-db"
-}
